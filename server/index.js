95ad40735be9f427e5c41d031e0fe13e15876237
Brian Olinger <brian.m.olinger77@gmail.com>
2026-05-17 16:06:51 -0400

    THE-85: Fix MongoStore import — destructure from connect-mongo v6

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const { MongoStore } = require('connect-mongo');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const path = require('path');
